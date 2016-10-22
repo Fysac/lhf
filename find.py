1c7756c864be1974b608fc605931d8e0f7e5552a
Fysac <Fysac@users.noreply.github.com>
2016-10-22 11:27:56 -0400

    Combine SSH banners into single query

--- find.py
+++ find.py
@@ -3,25 +3,21 @@ import shodan
 
 SHODAN_API_KEY = 'SHODAN_API_KEY'
 
-# Identifying SSH banners for Raspbian
-BANNERS = [
-    'SSH-2.0-OpenSSH_6.7p1 Raspbian-5+deb8u1',
-    'SSH-2.0-OpenSSH_6.7p1 Raspbian-5+deb8u2',
-    'SSH-2.0-OpenSSH_6.7p1 Raspbian-5+deb8u3'
-]
+QUERY = 'raspbian port:"22","2222"'
 
 def init_shodan():
     with open(SHODAN_API_KEY, 'r') as f:
         key = f.read().strip().replace('\n', '')
     return shodan.Shodan(key)
 
+# todo: Find workaround for large queries.
+# Shodan doesn't like it when max_results > 1000, gives:
+# shodan.exception.APIError: Unable to parse JSON response
 def search_shodan(max_results=100):
     api = init_shodan()
-
-    for banner in BANNERS:
-        results = api.search(banner, limit=(max_results / len(BANNERS)))
-        for r in results['matches']:
-            sys.stdout.write(r['ip_str'] + ':' + str(r['port']) + '\n')
+    results = api.search(QUERY, limit=max_results)
+    for r in results['matches']:
+        sys.stdout.write(r['ip_str'] + ':' + str(r['port']) + '\n')
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
