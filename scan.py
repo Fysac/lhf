c99dcd7b11120ccf34baef4cef1e84b80f454821
Fysac <Fysac@users.noreply.github.com>
2016-10-22 14:08:34 -0400

    Use queue to limit threads

--- scan.py
+++ scan.py
@@ -1,52 +1,55 @@
 import os
 import sys
 import paramiko
+import Queue
 from threading import Thread
 
-PARAMIKO_LOGGING = False
-USER = 'pi'
-PASS = 'raspberry'
-TIMEOUT = 10
+def ssh_login(q):
+    USERNAME = 'pi'
+    PASSWORD = 'raspberry'
 
-class SSHThread(Thread):
-    def __init__(self, host, port, username, password, timeout=10):
-        super(SSHThread, self).__init__()
-        self.host = host
-        self.port = port
-        self.username = username
-        self.password = password
-        self.timeout = timeout
-
-    def login(self):
+    while not q.empty():
+        (host, port) = q.get()
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
         try:
-            ssh.connect(self.host, port=int(self.port), username=self.username, password=self.password, allow_agent=False, look_for_keys=False, timeout=self.timeout)
+            ssh.connect(host, port=int(port), username=USERNAME, password=PASSWORD, allow_agent=False, look_for_keys=False, timeout=10)
         except Exception as e:
-            sys.stderr.write(self.host + ':' + self.port + ': ' + str(e) + '\n')
+            sys.stderr.write(host + ':' + port + ': ' + str(e) + '\n')
             sys.stderr.flush()
         else:
-            sys.stdout.write(self.host + ':' + self.port + '\n')
+            sys.stdout.write(host + ':' + port + '\n')
             sys.stdout.flush()
         finally:
             ssh.close()
+        q.task_done()
 
-    def run(self):
-        self.login()
+def scan(num_threads=100):
+    q = Queue.Queue()
 
-def scan():
     for line in sys.stdin.readlines():
         pi = line.split(':')
         (host, port) = (pi[0], pi[1].replace('\n', ''))
-        t = SSHThread(host, port, USER, PASS, TIMEOUT)
+        q.put((host, port))
+
+    for i in range(num_threads):
+        t = Thread(target=ssh_login, args=(q,))
+        t.daemon = True
         t.start()
 
+    q.join()
+
 if __name__ == '__main__':
-    if PARAMIKO_LOGGING:
+    LOGGING = False
+
+    if LOGGING:
         LOG_DIR = './log/'
         if not os.path.exists(LOG_DIR):
             os.makedirs(LOG_DIR)
         paramiko.util.log_to_file(LOG_DIR + 'paramiko.log')
 
-    scan()
+    if len(sys.argv) > 1:
+        scan(int(sys.argv[1]))
+    else:
+        scan()
